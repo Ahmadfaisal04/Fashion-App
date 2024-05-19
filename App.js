97fda88334ccf3df43e055a2011294fcc1558a77
Ahmadfaisal04 <ahmadfaizal0328@gmail.com>
2024-05-19 14:33:52 +0800

    tugas 2

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Image, View, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, flexDirection: 'column' }}>
-      <View style={{ flex: 1, flexDirection: 'row' }}>
-        <View style={{ flex: 1, backgroundColor: 'yellow' }}></View>
-        <View style={{ flex: 1, backgroundColor: 'green' }}></View>
-      </View>
-      <View style={{ flex: 1, backgroundColor: 'blue' }}></View>
+    <View style={{flex: 1,}}>
+        <View style={{flex: 1, flexDirection: 'row' }}>
+            <View style={{flex: 1,flexDirection: 'column'}}>
+              <View style={{flex: 1,backgroundColor: 'blue',}}></View>
+              <View style={{flex: 1,backgroundColor: 'lightblue',}}></View>
+            </View>
+            <View style={{flex: 1,flexDirection: 'column'}}>
+                  <View style={{flex: 1,flexDirection: 'row'}}>
+                    <View style={{flex: 1,backgroundColor: 'red',}}></View>
+                    <View style={{flex: 1,backgroundColor: 'orange',}}></View>
+                  </View>
+            </View>
+        </View>
+      <View style={{flex: 1,backgroundColor: 'pink',}}></View>
     </View>
   );
 }
