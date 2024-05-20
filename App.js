4158707ca0896265bb09f4569e4607747c4e5bfb
Ahmad Faisal <83072238+Ahmadfaisal04@users.noreply.github.com>
2024-05-20 14:50:43 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 
 export default function App() {
   return (
     <View style={{flex: 1,}}>
         <View style={{flex: 1, flexDirection: 'row' }}>
             <View style={{flex: 1,flexDirection: 'column'}}>
-              <View style={{flex: 1,backgroundColor: 'blue',}}></View>
-              <View style={{flex: 1,backgroundColor: 'lightblue',}}></View>
+              <View style={{flex: 1,backgroundColor: 'red',}}></View>
+              <View style={{flex: 1,backgroundColor: 'green',}}></View>
             </View>
             <View style={{flex: 1,flexDirection: 'column'}}>
                   <View style={{flex: 1,flexDirection: 'row'}}>
-                    <View style={{flex: 1,backgroundColor: 'red',}}></View>
-                    <View style={{flex: 1,backgroundColor: 'orange',}}></View>
+                    <View style={{flex: 1,backgroundColor: 'blue',}}></View>
+                    <View style={{flex: 1,backgroundColor: 'purple',}}></View>
                   </View>
             </View>
         </View>
-      <View style={{flex: 1,backgroundColor: 'pink',}}></View>
+      <View style={{flex: 1,backgroundColor: 'yellow',}}></View>
     </View>
   );
 }
